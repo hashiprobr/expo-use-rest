c29f10113dfd3507b306394351802ee57438b8af
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2022-01-18 20:14:59 -0300

    Refactor for web support

--- src/useRest.js
+++ src/useRest.js
@@ -1,9 +1,49 @@
 import { useState } from 'react';
 
+import { Platform } from 'react-native';
+
 import * as FileSystem from 'expo-file-system';
 
 export default function useRest(url) {
     const [running, setRunning] = useState(false);
+
+    function fail(status, type, message) {
+        if (type.startsWith('text/html')) {
+            const parser = new DOMParser();
+            const document = parser.parseFromString(message, 'text/html');
+            message = document.body.innerText.trim();
+        }
+        throw {
+            status: status,
+            message: message,
+        };
+    }
+
+    async function read(uri, init) {
+        let responseBody;
+        let response;
+        try {
+            response = await fetch(`${url}${uri}`, init);
+        } catch (error) {
+            throw {
+                status: 0,
+                message: error,
+            };
+        }
+        const status = response.status;
+        const type = response.headers.get('Content-Type');
+        if (status === 200) {
+            if (type.startsWith('application/json')) {
+                responseBody = await response.json();
+            } else {
+                responseBody = await response.text();
+            }
+        } else {
+            const message = await response.text();
+            fail(message);
+        }
+        return responseBody;
+    }
 
     async function request(method, uri, requestBody) {
         let responseBody;
@@ -20,34 +60,7 @@ export default function useRest(url) {
                     init.body = JSON.stringify(requestBody);
                 }
             }
-            let response;
-            try {
-                response = await fetch(`${url}${uri}`, init);
-            } catch (error) {
-                throw {
-                    status: 0,
-                    message: error,
-                };
-            }
-            const type = response.headers.get('Content-Type');
-            if (response.status === 200) {
-                if (type.startsWith('application/json')) {
-                    responseBody = await response.json();
-                } else {
-                    responseBody = await response.text();
-                }
-            } else {
-                let message = await response.text();
-                if (type.startsWith('text/html')) {
-                    const parser = new DOMParser();
-                    const document = parser.parseFromString(message, 'text/html');
-                    message = document.body.innerText.trim();
-                }
-                throw {
-                    status: response.status,
-                    message: message,
-                };
-            }
+            responseBody = await read(uri, init);
         } finally {
             setRunning(false);
         }
@@ -58,34 +71,37 @@ export default function useRest(url) {
         let responseBody;
         setRunning(true);
         try {
-            const options = { httpMethod: method };
-            let response;
-            try {
-                response = await FileSystem.uploadAsync(`${url}${uri}`, file, options);
-            } catch (error) {
-                throw {
-                    status: 0,
-                    message: error,
-                };
-            }
-            const type = response.headers['Content-Type'];
-            if (response.status === 200) {
-                if (type.startsWith('application/json')) {
-                    responseBody = JSON.parse(response.body);
+            if (Platform.OS === 'web') {
+                if (file.startsWith('data:')) {
+                    const init = { method: method };
+                    init.body = Buffer.from(file.slice(file.indexOf(',') + 1), 'base64');
+                    responseBody = await read(uri, init);
                 } else {
-                    responseBody = response.body;
+                    throw new Error('Upload in the web only supports data URLs');
                 }
             } else {
-                let message = response.body;
-                if (type.startsWith('text/html')) {
-                    const parser = new DOMParser();
-                    const document = parser.parseFromString(message, 'text/html');
-                    message = document.body.innerText.trim();
+                const options = { httpMethod: method };
+                let response;
+                try {
+                    response = await FileSystem.uploadAsync(`${url}${uri}`, file, options);
+                } catch (error) {
+                    throw {
+                        status: 0,
+                        message: error,
+                    };
                 }
-                throw {
-                    status: response.status,
-                    message: message,
-                };
+                const status = response.status;
+                const type = response.headers['Content-Type'];
+                if (status === 200) {
+                    if (type.startsWith('application/json')) {
+                        responseBody = JSON.parse(response.body);
+                    } else {
+                        responseBody = response.body;
+                    }
+                } else {
+                    const message = response.body;
+                    fail(message);
+                }
             }
         } finally {
             setRunning(false);
