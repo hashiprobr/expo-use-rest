2213c0e46bcb56d576732d280e78f7025bc2f058
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2022-05-06 11:16:38 -0300

    Add support for multipart

--- src/useRest.js
+++ src/useRest.js
@@ -7,96 +7,57 @@ import * as FileSystem from 'expo-file-system';
 export default function useRest(url) {
     const [running, setRunning] = useState(false);
 
-    async function read(uri, init) {
-        let responseBody;
-        let response;
-        try {
-            response = await fetch(`${url}${uri}`, init);
-        } catch (error) {
-            throw {
-                status: 0,
-                message: error,
-            };
-        }
-        const status = response.status;
-        const type = response.headers.get('Content-Type');
-        if (status === 200) {
-            if (type.startsWith('application/json')) {
-                responseBody = await response.json();
-            } else {
-                responseBody = await response.text();
-            }
-        } else {
-            const message = await response.text();
-            throw {
-                status: status,
-                message: message,
-            };
-        }
-        return responseBody;
-    }
-
-    async function request(method, uri, requestBody) {
+    async function request(method, uri, requestBody, files) {
         let responseBody;
         setRunning(true);
         try {
             const init = { method: method };
             if (method === 'POST' || method === 'PUT') {
-                init.headers = new Headers();
-                if (typeof requestBody === 'string') {
-                    init.headers.append('Content-Type', 'text/plain; charset=UTF-8');
-                    init.body = requestBody;
+                if (files) {
+                    init.body = new FormData();
+                    for (const [key, value] of Object.entries(files)) {
+                        let content;
+                        if (Platform.OS === 'web') {
+                            if (value.startsWith('data:')) {
+                                content = value.slice(value.indexOf(',') + 1);
+                            } else {
+                                throw new Error('Multipart in the web only supports data URIs');
+                            }
+                        } else {
+                            content = await FileSystem.readAsStringAsync(value, { encoding: FileSystem.EncodingType.Base64 });
+                        }
+                        init.body.append(key, new Blob([content], { type: 'application/octet-stream;base64' }));
+                    }
+                    init.body.append('body', new Blob([JSON.stringify(requestBody)], { type: 'application/json' }));
                 } else {
-                    init.headers.append('Content-Type', 'application/json; charset=UTF-8');
+                    init.headers = new Headers();
+                    init.headers.append('Content-Type', 'application/json');
                     init.body = JSON.stringify(requestBody);
                 }
             }
-            responseBody = await read(uri, init);
-        } finally {
-            setRunning(false);
-        }
-        return responseBody;
-    }
-
-    async function upload(method, uri, file) {
-        let responseBody;
-        setRunning(true);
-        try {
-            if (Platform.OS === 'web') {
-                if (file.startsWith('data:')) {
-                    const init = { method: method };
-                    const data = file.slice(file.indexOf(',') + 1);
-                    init.body = Buffer.from(data, 'base64');
-                    responseBody = await read(uri, init);
+            let response;
+            try {
+                response = await fetch(`${url}${uri}`, init);
+            } catch (error) {
+                throw {
+                    status: 0,
+                    message: error.message,
+                };
+            }
+            const status = response.status;
+            const type = response.headers.get('Content-Type');
+            if (status === 200) {
+                if (type.startsWith('application/json')) {
+                    responseBody = await response.json();
                 } else {
-                    throw new Error('Upload in the web only supports data URIs');
+                    responseBody = await response.text();
                 }
             } else {
-                const options = { httpMethod: method };
-                let response;
-                try {
-                    response = await FileSystem.uploadAsync(`${url}${uri}`, file, options);
-                } catch (error) {
-                    throw {
-                        status: 0,
-                        message: error.message,
-                    };
-                }
-                const status = response.status;
-                const type = response.headers['Content-Type'];
-                if (status === 200) {
-                    if (type.startsWith('application/json')) {
-                        responseBody = JSON.parse(response.body);
-                    } else {
-                        responseBody = response.body;
-                    }
-                } else {
-                    const message = response.body;
-                    throw {
-                        status: status,
-                        message: message,
-                    };
-                }
+                const message = await response.text();
+                throw {
+                    status: status,
+                    message: message,
+                };
             }
         } finally {
             setRunning(false);
@@ -107,10 +68,8 @@ export default function useRest(url) {
     return {
         running,
         get: (uri) => request('GET', uri),
-        post: (uri, requestBody) => request('POST', uri, requestBody),
-        put: (uri, requestBody) => request('PUT', uri, requestBody),
+        post: (uri, requestBody, files) => request('POST', uri, requestBody, files),
+        put: (uri, requestBody, files) => request('PUT', uri, requestBody, files),
         delete: (uri) => request('DELETE', uri),
-        uploadPost: (uri, file) => upload('POST', uri, file),
-        uploadPut: (uri, file) => upload('PUT', uri, file),
     };
 }
